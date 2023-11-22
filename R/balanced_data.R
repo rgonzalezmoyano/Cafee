1c75552da3d01e73bba41a223c43ce697f02a068
Victor-Espana <vespana@umh.es>
2023-11-22 10:22:39 +0100

    Hemos roto github por segunda vez

---
+++ R/balanced_data.R
@@ -0,0 +1,307 @@
+#' @title Create New DMUs to Balance Data
+#'
+#' @description This function creates new DMUs to address data imbalances. If the majority class is efficient, it generates new inefficient DMUs by worsering the observed units. Conversely, if the majority class is inefficient, it projects inefficient DMUs to the frontier. Finally, a random selection if performed to keep a proportion of 0.65 for the majority class and 0.35 for the minority class.
+#' 
+#' @param data A \code{data.frame} containing the variables used in the model.
+#' @param x Column indexes of the input variables in the \code{data}.
+#' @param y Column indexes of the output variables in the \code{data}.
+#'
+#' @return It returns a \code{data.frame} with the newly created set of DMUs incorporated.
+
+balance_data <- function (
+      data, x, y
+    ) {
+  
+  # number of inputs
+  nX <- length(x)
+  
+  # number of outputs
+  nY <- length(y)
+  
+  # number of samples
+  N <- nrow(data)
+  
+  # ================== #
+  # enough sample size #
+  # ================== #
+  
+  if (N < 100) {
+    
+    # create new "n" observations
+    grow_n <- 100 - N
+    
+    # create new inefficient observations
+    ineff_dmu <- create_dmu (
+      data = data,
+      x = x,
+      y = y,
+      N = grow_n / 2,
+      type = "inefficient"
+    )
+    
+    # create efficient observations
+    eff_dmu <- create_dmu (
+      data = data,
+      x = x,
+      y = y,
+      N = grow_n / 2,
+      type = "efficient"
+    )
+    
+    data <- rbind(data, ineff_dmu, eff_dmu)
+    data <- data[complete.cases(data), ]
+  }
+  
+  # ============== #
+  # sub - frontier #
+  # ============== #
+  
+  # frontier
+  eff_data <- data[data$class_efficiency == "efficient", ]
+  
+  # perturbations
+  perturbations <- data.frame(matrix(runif(nrow(eff_data) * (ncol(eff_data) - 1), 0, 0.1), nrow = nrow(eff_data)))
+  
+  # sub - frontier data
+  sfd_data <- data.frame (
+    eff_data[, x] + eff_data[, x] * perturbations[, x],
+    eff_data[, y] - eff_data[, y] * perturbations[, y],
+    "class_efficiency" = "not_efficient"
+  )
+  
+  colnames(sfd_data) <- colnames(data)
+  
+  data <- rbind(data, sfd_data)
+
+  # =================== #
+  # balance proportions #
+  # =================== #
+
+  # proportions
+  props <- prop.table(table(data$class_efficiency))
+  
+  # proportion of dmus efficient
+  prop_eff <- props["efficient"]
+  
+  # proportion of dmus not efficient
+  prop_ineff <- props["not_efficient"]
+  
+  # number of dmus efficient
+  n_eff <- prop_eff * nrow(data)
+  
+  # number of dmus not efficient
+  n_ineff <- prop_ineff * nrow(data)
+  
+  if (prop_eff > prop_ineff) {
+    
+    # ======================= #
+    # create inefficient DMUs #
+    # ======================= #
+    
+    # number of dmus to create
+    new_dmus <- ceiling(((- 0.50 * n_ineff) + (0.50 * n_eff)) / 0.50)
+    
+    # create new inefficient observations
+    ineff_dmu <- create_dmu (
+      data = data,
+      x = x,
+      y = y,
+      N = new_dmus,
+      type = "inefficient"
+    )
+    
+    data <- rbind(data, ineff_dmu)
+
+  } else {
+    
+    # ===================== #
+    # create efficient DMUs #
+    # ===================== #
+    
+    # number of dmus to create
+    new_dmus <- ceiling(((- 0.50 * n_eff) + (0.50 * n_ineff)) / 0.50)
+    
+    # create new efficient observations
+    eff_dmu <- create_dmu (
+      data = data,
+      x = x,
+      y = y,
+      N = new_dmus,
+      type = "efficient"
+    )
+    
+    data <- rbind(data, eff_dmu)
+    
+  }
+
+  return(data)
+}
+
+#' @title Create New DMUs to reach 150 dmus
+#'
+#' @description This function adds DMUs to reach a sample of 150.
+#' 
+#' @param data A \code{data.frame} containing the variables used in the model.
+#' @param x number of inputs in the \code{data}.
+#' @param y number of outputs in the \code{data}.
+#' @param N number of dmus to create \code{data}.
+#' @param type Class of dmu to create \code{data}.
+#'
+#' @return It returns a \code{data.frame} with the newly created set of DMUs incorporated.
+
+create_dmu <- function (
+    data, x, y, N, type
+    ) {
+  
+  # number of inputs
+  nX <- length(x)
+  
+  # number of outputs
+  nY <- length(y)
+  
+  if (type == "inefficient") {
+    
+    # ======================= #
+    # create inefficient DMUs #
+    # ======================= #
+    
+    new_dmus <- N
+  
+    # indexes of DMUs for worsening
+    if (new_dmus > nrow(data)) {
+      replace <- TRUE
+    } else {
+      replace <- FALSE
+    }
+    idx_dmu_change <- sample(1:nrow(data), size = new_dmus, replace = replace)
+    
+    # create a new matrix of data
+    new_dmu_values <- matrix(data = NA, nrow = new_dmus, ncol = nX + nY)  
+    colnames(new_dmu_values) <- names(data)[c(x, y)]
+    
+    # data is moved by a uniform distribution.
+    # minimum parameter for the uniform distribution.
+    min_unif <- 0
+    
+    # alteration of inputs
+    # maximum values of inputs
+    max_value_x <- apply(X = data[x], MARGIN = 2, FUN = max)
+    
+    for (i in 1:new_dmus) {
+      
+      # select a specific DMU
+      dmu <- idx_dmu_change[i]
+      
+      # maximum parameter for the uniform distribution
+      max_unif <- max_value_x - data[dmu, x]
+      
+      # create new DMUs
+      for (j in x) {
+        make_inefficient <- runif(n = 1, min = min_unif, max = as.numeric(max_unif[j]))
+        new_dmu_values[i, j] <- data[dmu, j] + make_inefficient
+      }
+      
+    }
+    
+    # alteration of outputs
+    # minimum values of outputs
+    min_value_y <- apply(X = as.matrix(data[, y]), MARGIN = 2, FUN = min)
+    
+    for (i in 1:new_dmus) {
+      
+      # select a specific DMU
+      dmu <- idx_dmu_change[i]
+      
+      # maximum parameter for the uniform distribution
+      max_unif <- data[dmu, y] - min_value_y
+      
+      for (j in 1:nY) {
+        make_inefficient <- runif(n = 1, min = min_unif, max = as.numeric(max_unif[j]))
+        new_dmu_values[i, nX + j] <- data[dmu, nX + j] - make_inefficient
+      }
+      
+    }
+    
+    # new set of DMUs to data.frame
+    new_dmu_values <- as.data.frame(new_dmu_values)
+    
+    # classification of the new dmus as "inefficient"
+    new_dmu_values$class_efficiency <- "not_efficient"
+    
+  } else {
+    
+    # ===================== #
+    # create efficient DMUs #
+    # ===================== #
+    
+    # select the minimum number of additions required to balance the data
+    new_dmus <- N
+    
+    # compute bcc_scores
+    bcc_scores_out <- rad_out (
+      tech_xmat = as.matrix(data[, x]),
+      tech_ymat = as.matrix(data[, y]),
+      eval_xmat = as.matrix(data[, x]),
+      eval_ymat = as.matrix(data[, y]),
+      convexity = TRUE,
+      returns = "variable"
+    ) 
+    
+    bcc_scores_inp <- rad_inp (
+      tech_xmat = as.matrix(data[, x]),
+      tech_ymat = as.matrix(data[, y]),
+      eval_xmat = as.matrix(data[, x]),
+      eval_ymat = as.matrix(data[, y]),
+      convexity = TRUE,
+      returns = "variable"
+    )
+    
+    # efficient DMUs
+    idx_eff <- c(1:nrow(data))[bcc_scores_out - 1 < 0.001]
+  
+    # compute 1st decile
+    rows_fst_dec <- matrix(0, nrow = nrow(data), ncol = ncol(data) - 1)
+    
+    for (i in 1:ncol(rows_fst_dec)) {
+      rows_fst_dec[, i] <- data[, i] <= quantile(data[, i], probs = 0.25)
+    }
+    
+    # observations in the 1st decil
+    rows_fst_dec <- c(1:nrow(rows_fst_dec))[rowSums(rows_fst_dec) == 2]
+    
+    # select the indexes to input projection
+    idx_inp <- setdiff(rows_fst_dec, idx_eff)
+    
+    # select the indexes to output projection
+    size = min(length(setdiff(c(1:nrow(data)), c(idx_inp, idx_eff))), new_dmus - length(idx_inp))
+    
+    idx_out <- sample(setdiff(c(1:nrow(data)), c(idx_inp, idx_eff)), size = size)
+    
+    # project inefficient data 
+    proj_data_out <- as.data.frame(cbind(data[, x], data[, y] * bcc_scores_out[, 1]))
+    proj_data_inp <- as.data.frame(cbind(data[, x] * bcc_scores_inp[, 1], data[, y]))
+    
+    names(proj_data_out) <- names(data[, c(x, y)])
+    names(proj_data_inp) <- names(data[, c(x, y)])
+    
+    proj_data_out$class_efficiency <- "efficient"
+    proj_data_inp$class_efficiency <- "efficient"
+    
+    # create DMUs by orientation
+    new_dmu_values <- as.data.frame(matrix(data = NA, nrow = new_dmus, ncol = nX + nY + 1)) 
+    names(new_dmu_values) <- names(data)
+    
+    # projection
+    for (i in 1:new_dmus) {
+      dmu <- c(idx_inp, idx_out)[i]
+      
+      if (dmu %in% idx_out) {
+        new_dmu_values[i, ] <- proj_data_out[dmu, ]
+      } else {
+        new_dmu_values[i, ] <- proj_data_inp[dmu, ]
+      }
+    }
+  }
+  
+  return(new_dmu_values)
+}
---
+++ R/efficiency_estimation.R
@@ -0,0 +1,285 @@
+#' @title Efficiency Estimation Using Classification Algorithms
+#'
+#' @description This function uses classification algorithms to estimate the efficiency of a set of DMUs (Decision Making Units).
+#'
+#' @param data A \code{data.frame} or \code{matrix} containing the variables in the model.
+#' @param x Column indexes of input variables in \code{data}.
+#' @param y Column indexes of output variables in \code{data}.
+#' @param orientation A \code{string}, equal to \code{"input"} (input-oriented) or \code{"output} (output-oriented)
+#' @param trControl Parameters for controlling the training process (from the \code{'caret'} package).
+#' @param methods A \code{list} of selected machine learning models and their hyperparameters.
+#' @param metric A \code{string} specifying the summary metric for classification to select the optimal model. Default includes \code{"Balanced_accuracy"} due to (normally) unbalanced data.
+#' @param hold_out A \code{number} value (5-20) for validation data percentage during training (default: 0.2).
+#'
+#' @importFrom caret trainControl train createDataPartition defaultSummary prSummary
+#' @importFrom dplyr select_if %>% arrange top_n sample_n
+#' @importFrom Benchmarking dea.boot
+#'
+#' @return A \code{"cafee"} object.
+#'
+#' @export
+
+efficiency_estimation <- function (
+    data, x, y, orientation,
+    trControl, methods, metric, hold_out
+    ) {
+
+  # pre-processing
+  data <- preprocessing (
+    data = data, 
+    x = x, 
+    y = y
+    )
+
+  # reorder index 'x' and 'y' in data
+  x <- 1:(ncol(data) - length(y))
+  y <- (length(x) + 1):ncol(data)
+  
+  # number of inputs / outputs as inputs and number of outputs
+  nX <- length(x)
+  nY <- length(y)
+  
+  # compute DEA scores through bootstrapping
+  # bootstrapping_dea <- dea.boot (
+  #   as.matrix(data[, x]), 
+  #   as.matrix(data[, y]), 
+  #   NREP = 200
+  # )
+  # 
+  # new_inp <- data[, x] * bootstrapping_dea[["eff.bc"]]
+  # 
+  # # no overfitted data
+  # no_data <- as.data.frame (
+  #   matrix (
+  #     c(new_inp, data[, y]), 
+  #     nrow = nrow(data),
+  #     ncol = length(x) + length(y)
+  #     )
+  # )
+  # 
+  # colnames(no_data) <- colnames(data)
+  
+  # compute DEA scores through an additive model
+  add_scores <- compute_scores_additive (
+    data = no_data, 
+    x = x, 
+    y = y
+    )
+  
+  # determine efficient and inefficient DMUs
+  class_efficiency <- ifelse(add_scores[, 1] <= 0.0001, 1, 0)
+  
+  data <- cbind(data, class_efficiency) %>% as.data.frame()
+  data$class_efficiency <- factor(data$class_efficiency)
+  data$class_efficiency <- factor (
+    data$class_efficiency, 
+    levels = rev(levels(data$class_efficiency))
+  )
+  
+  levels(data$class_efficiency) <- c("efficient", "not_efficient")
+  
+  # save a copy of the original data
+  eval_data <- data
+  
+  # observed proportion of efficient and inefficient DMUs.
+  obs_prop <- prop.table(table(data$class_efficiency))
+
+  # check presence of imbalanced data
+  if (max(obs_prop[1], obs_prop[2]) > 0.50 | nrow(data) < 150) {
+    data <- balance_data (
+      data = data, 
+      x = x, 
+      y = y
+    )
+  }
+
+  # Create train and validation data
+  valid_index <- createDataPartition (
+    data$class_efficiency,
+    p = hold_out,
+    list = FALSE
+    )
+
+  valid_data <- data[valid_index, ]
+  train_data <- data[- valid_index, ]
+  
+  # ====================== #
+  # SELECT HYPERPARAMETERS #
+  # ====================== #
+  
+  ml_model <- train_ml (
+    data = train_data,
+    trControl = trControl,
+    methods = methods
+    )
+
+  # Best training 
+  confusion_matrix <- vector("list", length = length(methods))
+  names(confusion_matrix) <- names(methods)
+  
+  parms_vals <- vector("list", length = length(methods))
+  names(parms_vals) <- names(methods)
+  
+  # ================= #
+  # SELECT BEST MODEL #
+  # ================= #
+  
+  for (i in 1:length(methods)) {
+    
+    # parameter position
+    parms_posn <- which(names(ml_model[[i]]) %in% names(methods[[i]])) 
+    
+    # parameter values
+    parms_vals[[i]] <- as.data.frame(ml_model[[i]])[, parms_posn]
+    
+    # rename parameters if is null
+    if (is.null(names(parms_vals[[i]]))) {
+      parms_vals[[i]] <- as.data.frame(parms_vals[[i]])
+      names(parms_vals[[i]]) <- names(methods[[i]])
+    }
+    
+    # avoid messages for some methods
+    verb_methods <- c("gbm", "svmPoly")
+    
+    if (names(methods[i]) %in% verb_methods) {
+      # tune models
+      best_ml_model <- train (
+        form = class_efficiency ~.,
+        data = train_data,
+        method = names(methods[i]),
+        tuneGrid = parms_vals[[i]],
+        verbose = FALSE
+      )
+      
+    } else {
+      # Tune models
+      best_ml_model <- train (
+        form = class_efficiency ~.,
+        data = train_data,
+        method = names(methods[i]),
+        tuneGrid = parms_vals[[i]]
+      )
+    }
+    
+    y_obs <- valid_data$class_efficiency
+    y_hat <- predict(best_ml_model, valid_data)
+    
+    #create confusion matrix and calculate metrics related to confusion matrix
+    confusion_matrix[[i]] <- confusionMatrix (
+      data = y_hat,
+      reference = y_obs,
+      mode = "everything",
+      positive = "efficient"
+      )[["byClass"]]
+  }
+  
+  # matrix for model evaluation
+  precision_models <- matrix (
+    nrow = length(methods),
+    ncol = length(names(confusion_matrix[[1]]))
+  )
+  
+  precision_models <- as.data.frame(precision_models)
+  
+  # names of precision_models matrix
+  colnames(precision_models) <- names(confusion_matrix[[1]])
+  rownames(precision_models) <- names(methods)
+  
+  for (i in 1:length(methods)) {
+    precision_models[i, ] <- confusion_matrix[[i]]
+  }
+
+  # select the best model by metric
+  selected_model <- precision_models %>%
+    arrange(desc(F1), desc(Sensitivity), desc("Balanced Accuracy"))
+  selected_model <- selected_model[1, ]
+  
+  # index of the best model in ml_model
+  best_model_index <- which(row.names(selected_model) == names(ml_model))
+  
+  # ============== #
+  # FIT BEST MODEL #
+  # ============== #
+  
+  # avoid messages for some methods
+  verb_methods <- c("gbm", "svmPoly")
+  
+  if (names(methods[i]) %in% verb_methods) {
+    final_model <- train (
+      form = class_efficiency ~.,
+      data = data,
+      method = row.names(selected_model),
+      tuneGrid = parms_vals[[best_model_index]],
+      verbose = FALSE,
+      trControl = trainControl(classProbs = TRUE)
+    )
+  } else {
+    final_model <- train (
+      form = class_efficiency ~.,
+      data = data,
+      method = row.names(selected_model),
+      tuneGrid = parms_vals[[best_model_index]],
+      trControl = trainControl(classProbs = TRUE)
+    )
+  }
+
+  cut_off <- select_cut_off (
+    data = valid_data,
+    final_model = final_model
+  )
+  
+  final_model$cut_off <- cut_off
+  
+  return(final_model)
+}
+
+#' @title Select cut-off point in Classification Algorithm
+#'
+#' @description This function selects the cut-off point that minimizes the sum of false positives and false negatives.
+#' 
+#' @param data A \code{data.frame} or \code{matrix} containing the variables in the model.
+#' @param final_model The best \code{train} object from \code{caret}.
+#'
+#' @return It returns the best cut-off point.
+
+select_cut_off <- function (
+    data, final_model
+    ) {
+  
+  # cut-off points
+  df_cp <- data.frame (
+    cut_off_points = seq(0, 1, by = 0.01),
+    false_pred = NA
+  )
+  
+  # predictions
+  y_hat <- predict(final_model, data, type = "prob")
+  
+  for (i in 1:nrow(df_cp)) {
+    
+    # cut-off point
+    cp_point <- df_cp[i, "cut_off_points"]
+    
+    # predictions for a given cut-off point
+    y_hat_cp <- ifelse(y_hat$efficient >= cp_point, "efficient", "not_efficient")
+    y_hat_cp <- factor(y_hat_cp, levels = c("efficient", "not_efficient"))
+    
+    # confusion matrix
+    cm_cp <- confusionMatrix (
+      data = y_hat_cp,
+      reference = data$class_efficiency,
+      mode = "everything",
+      positive = "efficient"
+    )[["table"]]
+    
+    # compute false positive and false negative
+    df_cp[i, "false_pred"] <- cm_cp[2, 1] + cm_cp[1, 2]
+  }
+
+  # minimum cost for the cut-off point
+  min_value <- min(df_cp$false_pred)
+  min_index <- which(df_cp$false_pred == min_value)
+  cut_point <- df_cp[max(min_index), "cut_off_points"]
+  
+  return(cut_point)
+}
