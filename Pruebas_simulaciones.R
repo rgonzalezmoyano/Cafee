799217d0b09db6a2da10d93a3272b70c1cdd5010
rgonzalezmoyano <125368117+rgonzalezmoyano@users.noreply.github.com>
2024-01-30 16:52:19 +0100

    graficar correlaciones

--- Pruebas_simulaciones.R
+++ Pruebas_simulaciones.R
@@ -17,10 +17,54 @@ data2 <- simulaciones[simulaciones$noise == 0, ]
 
 data_filtrado <- data2[c("scenario", "N", "ratio_obs_por_input", "corr_yD_DEA", "corr_yD_BDEA", "corr_yD_cafee_DEA", "corr_yD_cafee_BDEA")]
 
+data_DEA <- data2[c("scenario", "N", "ratio_obs_por_input", "corr_yD_DEA")]
+data_DEA$corr <- "corr_yD_DEA"
+names(data_DEA)[4] <- "value"
+
+data_BDEA <- data2[c("scenario", "N", "ratio_obs_por_input", "corr_yD_BDEA")]
+data_BDEA$corr <- "corr_yD_BDEA"
+names(data_BDEA)[4] <- "value"
+
+data_cafee_DEA <- data2[c("scenario", "N", "ratio_obs_por_input", "corr_yD_cafee_DEA")]
+data_cafee_DEA$corr <- "corr_yD_cafee_DEA"
+names(data_cafee_DEA)[4] <- "value"
+
+data_cafee_BDEA <- data2[c("scenario", "N", "ratio_obs_por_input", "corr_yD_cafee_BDEA")]
+data_cafee_BDEA$corr <- "corr_yD_cafee_BDEA"
+names(data_cafee_BDEA)[4] <- "value"
+
+new_data <- rbind(data_DEA, data_BDEA, data_cafee_DEA, data_cafee_BDEA)
+
+par(mfrow = c(1,1))
+  
+  ggplot(data = new_data) +
+    geom_point(aes(x = ratio_obs_por_input, y = value, colour = scenario)) +
+    geom_line(aes(x = ratio_obs_por_input, y = value, group = scenario, colour = scenario)) +
+    scale_y_continuous(limits = c(0, 1)) +
+    facet_wrap(~corr) +
+    theme_bw() +
+    theme(
+      axis.title.x = element_text (
+        size = 12, face = "bold", color = "#921F30",
+        margin = margin(t = 10)),
+      axis.title.y = element_text (
+        size = 12, face = "bold", color = "#921F30",
+        margin = margin(r = 10)),
+      axis.text = element_text (
+        size = 12, color = "black"),
+      plot.margin = unit(c(1.25, 1.25, 1.25, 1.25), "lines"),
+      plot.title = element_text (
+        size = 12, face = "bold", color = "#921F30",
+        margin = margin(b = 10)
+      ),
+      legend.position = "right"
+    )
+
+
 
 ggplot(data = data_filtrado) +
-  geom_point(aes(x = ratio_obs_por_input, y = corr_yD_DEA, colour = scenario)) +
-  geom_line(aes(x = ratio_obs_por_input, y = corr_yD_DEA, group = scenario, colour = scenario)) +
+  geom_point(aes(x = ratio_obs_por_input, y = corr_yD_BDEA, colour = scenario)) +
+  geom_line(aes(x = ratio_obs_por_input, y = corr_yD_BDEA, group = scenario, colour = scenario)) +
   scale_y_continuous(limits = c(0, 1)) +
   theme_bw() +
   theme(
@@ -39,6 +83,8 @@ ggplot(data = data_filtrado) +
     ),
     legend.position = "right"
   )
+
+
 
 
 ggplot() + 
