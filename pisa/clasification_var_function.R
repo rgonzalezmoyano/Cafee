688c364cbb27299450d9f1833e34b1663e0d3424
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2024-03-05 16:28:49 +0100

    detectar liker 1.0

--- pisa/clasification_var_function.R
+++ pisa/clasification_var_function.R
@@ -10,12 +10,16 @@ library(dplyr)
 # Load data #
 # ========= #
 load("C:/Users/Ricardo/Downloads/PISA_2012.RData")
-data_PISA2012 <- a$data
-
-data <- data_PISA2012[1:10, ]
+data <- a$data
 
 value_detection <- function (data) {
   
-  data_
-  nu
+  data_dim <- matrix (
+    ncol = ncol(data),
+    nrow = 1
+  )
+  
+  data_dim <- apply(data, MARGIN = 2, FUN = function(x) length(unique(x)))
+  
+  idx_liker <- which(data_dim < 10)
 }
