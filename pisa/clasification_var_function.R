656f2d8b7ba9658452f076751225bac8a1aa398f
rgonzalezmoyano <125368117+rgonzalezmoyano@users.noreply.github.com>
2024-03-23 15:34:26 +0100

    new changes

--- pisa/clasification_var_function.R
+++ pisa/clasification_var_function.R
@@ -13,6 +13,43 @@ load("C:/Users/Ricardo/Downloads/PISA_2012.RData")
 data <- a$data
 info <- a$names
 
+# dimensions
+nrow <- length(unique(data$SCHOOLID))
+ncol <- as.numeric(length(names(data))) - 1 # No STIDSTD 
+
+new_data <- matrix(
+  data = NA,
+  ncol = ncol,
+  nrow = nrow
+  )
+
+new_data <- as.data.frame(new_data)
+
+# First: ID information
+for (i in 1:5) {
+  data[[i]] <- as.factor(data[[i]])
+  print(paste(info$vble[i], "is", info$name[i]))
+}
+
+for (i in 6:7) {
+  data[[i]] <- as.numeric(data[[i]])
+  print(paste(info$vble[i], "is", info$name[i]))
+}
+
+# in
+info_data <- data[, 1:7]
+
+info_data <- info_data[-7] %>% 
+  group_by(SCHOOLID) %>% 
+  
+
+
+
+
+
+
+
+
 value_detection <- function (data) {
   
   data_dim <- matrix (
