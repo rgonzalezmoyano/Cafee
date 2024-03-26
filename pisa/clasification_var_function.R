6668d38a455e4cc82aee20c6a68e404f539e07fb
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2024-03-26 16:25:39 +0100

    clasificar variables: hasta 420

--- pisa/clasification_var_function.R
+++ pisa/clasification_var_function.R
@@ -13,56 +13,37 @@ load("C:/Users/Ricardo/Downloads/PISA_2012.RData")
 data <- a$data
 info <- a$names
 
-<<<<<<< HEAD
 # assign factor variable
-idx_change_factor <- c(19)
+idx_change_factor <- c(4, 9, 10, 11, 12, 14, 21:26, 28:31, 34:37, 39, 40:60, 120:124,
+                       133:136,  147:171, 284:301, 333:339, 348:376, 378:387, 389:390,
+                       392, 408:410, 412:413)
 
 # determinate likert variable
-likert <- c(19)
+likert <- c(8, 13, 15:20, 27, 32:33, 38, 61:119, 125:132, 137:140, 180:283, 302:332,
+            340:347, 377, 388, 391, 393:404)
 
 # numeric variables 
-idx_change_num
+idx_change_num <- c(141:146, 172:179, 405:407, 411, 414:420)
 
-# change to factor
-for(factor_idx in idx_change_factor) {
-  data[[factor_idx]] <- as.factor(data[[factor_idx]])
+# Transform variable to YES == 1 and NO == 2
+get_last_char <- function(row) {
+  last_char <- substr(row, nchar(row), nchar(row))
+  return(last_char)
 }
 
-# ======================== #
-# define type of variables #
-# ======================== #
-=======
-# dimensions
-nrow <- length(unique(data$SCHOOLID))
-ncol <- as.numeric(length(names(data))) - 1 # No STIDSTD 
-
-new_data <- matrix(
-  data = NA,
-  ncol = ncol,
-  nrow = nrow
-  )
-
-new_data <- as.data.frame(new_data)
-
-# First: ID information
->>>>>>> 2684bbeb7c354a6938fa625a5a0223d86c726ac0
-for (i in 1:5) {
-  data[[i]] <- as.factor(data[[i]])
-  print(paste(info$vble[i], "is", info$name[i]))
-}
-
-<<<<<<< HEAD
-a <- as.factor(data$CNT)
-
+data$ST26Q15 <- apply(as.matrix(data$ST26Q15), MARGIN = 1, FUN = get_last_char)
+data$ST26Q16 <- apply(as.matrix(data$ST26Q16), MARGIN = 1, FUN = get_last_char)
+data$ST26Q17 <- apply(as.matrix(data$ST26Q17), MARGIN = 1, FUN = get_last_char)
 
 # =============== #
 # group_by school #
 # =============== #
+
 # create ID_key
-data$ID_PISA <- paste0(data$CNT,data$SCHOOLID)
+data$ID_PISA <- paste0(data$CNT, data$SCHOOLID)
 
 # dimensions
-nrow = length(unique(data$SCHOOLID))
+nrow = length(unique(data$ID_PISA))
 ncol <- as.numeric(length(names(data)))
 
 
@@ -74,6 +55,9 @@ new_data <- matrix (
 )
 
 new_data <- as.data.frame(new_data)
+
+# names
+names(new_data) <- names(data)
 
 # bucle
 for (i in 1:nrow(new_data)) {
@@ -92,7 +76,10 @@ for (i in 1:nrow(new_data)) {
   
   # STRATUM
   new_data[i, 3] <- unique(i_data$STRATUM)
-
+  
+  # OECD
+  new_data[i, 4] <- unique(i_data$OECD)
+  
   # NC
   new_data[i, 5] <- unique(i_data$NC)
   
@@ -101,7 +88,7 @@ for (i in 1:nrow(new_data)) {
   
   # STIDSTD
   # ID information, after group_by, delete
-  new_data[i, 7] <- unique(i_data$STIDSTD)[1]
+  new_data[i, 7] <- NA
   
   for (vble in 1:ncol(i_data)) {
     
@@ -110,16 +97,24 @@ for (i in 1:nrow(new_data)) {
     
     posible_value <- sort(as.numeric(posible_value))
     
-    # by default
-    strange_v <- 1
-    
-    if (posible_value[length(posible_value)] - posible_value[length(posible_value) - 1] == 1) {
+    if (vble %in% c(409)) {
       
-      strange_v <- 2
+      strange_v <- 0
       
-      if (posible_value[length(posible_value)] - posible_value[length(posible_value) - 2] == 2) {
+    } else {
+      
+      # by default
+      strange_v <- 1
+      
+      if (posible_value[length(posible_value)] - posible_value[length(posible_value) - 1] == 1) {
         
-        strange_v <- 3
+        strange_v <- 2
+        
+        if (posible_value[length(posible_value)] - posible_value[length(posible_value) - 2] == 2) {
+          
+          strange_v <- 3
+          
+        }
         
       }
       
@@ -172,55 +167,56 @@ for (i in 1:nrow(new_data)) {
     
   }
   
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-for (i in 6:7) {
-  data[[i]] <- as.numeric(data[[i]])
-  print(paste(info$vble[i], "is", info$name[i]))
-}
-
-# in
-info_data <- data[, 1:7]
-
-info_data <- info_data[-7] %>% 
-  group_by(SCHOOLID) %>% 
-  
->>>>>>> 2684bbeb7c354a6938fa625a5a0223d86c726ac0
-
-
-
-
-
-
-
-
-value_detection <- function (data) {
-  
-  data_dim <- matrix (
-    ncol = ncol(data),
-    nrow = 1
-  )
-  
-  data_dim <- apply(data, MARGIN = 2, FUN = function(x) length(unique(x)))
-  
-  idx_liker <- which(data_dim < 10)
-  
-  data_lkr <- data[idx_liker]
-  
   
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for (i in 6:7) {
+#   data[[i]] <- as.numeric(data[[i]])
+#   print(paste(info$vble[i], "is", info$name[i]))
+# }
+# 
+# # in
+# info_data <- data[, 1:7]
+# 
+# info_data <- info_data[-7] %>% 
+#   group_by(SCHOOLID) %>% 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# value_detection <- function (data) {
+#   
+#   data_dim <- matrix (
+#     ncol = ncol(data),
+#     nrow = 1
+#   )
+#   
+#   data_dim <- apply(data, MARGIN = 2, FUN = function(x) length(unique(x)))
+#   
+#   idx_liker <- which(data_dim < 10)
+#   
+#   data_lkr <- data[idx_liker]
+#   
+#   
+#   
+# }
 
