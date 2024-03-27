e90e8d488311a468dfc11bdc195e8ebfc05df42a
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2024-03-27 17:36:05 +0100

    indices

--- pisa/clasification_var_function.R
+++ pisa/clasification_var_function.R
@@ -16,14 +16,20 @@ info <- a$names
 # assign factor variable
 idx_change_factor <- c(4, 9, 10, 11, 12, 14, 21:26, 28:31, 34:37, 39, 40:60, 120:124,
                        133:136,  147:171, 284:301, 333:339, 348:376, 378:387, 389:390,
-                       392, 408:410, 412:413)
+                       392, 408:410, 412:413, 421:423, 435, 439, 448, 456:459, 468:469,
+                       483, 632, 634, 635)
 
 # determinate likert variable
 likert <- c(8, 13, 15:20, 27, 32:33, 38, 61:119, 125:132, 137:140, 180:283, 302:332,
-            340:347, 377, 388, 391, 393:404)
+            340:347, 377, 388, 391, 393:404, 436, 439, 454:455, 465)
 
 # numeric variables 
-idx_change_num <- c(141:146, 172:179, 405:407, 411, 414:420)
+idx_change_num <- c(141:146, 172:179, 405:407, 411, 414:420, 424:434, 437:438, 440:447,
+                    449:453, 460:464, 466, 467, 470:476, 477:482, 484:499, 500:631, 633)
+
+# test and validation variables
+all_vector <-c(idx_change_factor, likert, idx_change_num)
+diff <- setdiff(1:635, all_vector)
 
 # Transform variable to YES == 1 and NO == 2
 get_last_char <- function(row) {
@@ -60,8 +66,8 @@ new_data <- as.data.frame(new_data)
 names(new_data) <- names(data)
 
 # bucle
-for (i in 1:nrow(new_data)) {
-  
+for (i in 23:nrow(new_data)) {
+  print(i)
   # ID
   new_data[i, 635] <- unique(data$ID_PISA)[i]
   
@@ -90,29 +96,39 @@ for (i in 1:nrow(new_data)) {
   # ID information, after group_by, delete
   new_data[i, 7] <- NA
   
-  for (vble in 1:ncol(i_data)) {
+  for (vble in 8:ncol(i_data)) {
     
     # determinate if exist strange range
-    posible_value <- unique(i_data[[vble]])
+    posible_value <- unique(data[[vble]])
     
     posible_value <- sort(as.numeric(posible_value))
     
-    if (vble %in% c(409)) {
+    if (vble %in% c(11, 12, 409, 410, 457, 459, 500:630, 634, 635)) {
       
       strange_v <- 0
-      
+      no_value <- 0
+
     } else {
       
       # by default
       strange_v <- 1
       
+      # get value to delete per school
+      no_value <- posible_value[length(posible_value)]
+      
       if (posible_value[length(posible_value)] - posible_value[length(posible_value) - 1] == 1) {
         
         strange_v <- 2
         
+        no_value_1 <- posible_value[length(posible_value) - 1]
+        no_value <- c(no_value, no_value_1)
+        
         if (posible_value[length(posible_value)] - posible_value[length(posible_value) - 2] == 2) {
           
           strange_v <- 3
+          
+          no_value_2 <- posible_value[length(posible_value) - 2]
+          no_value <- c(no_value, no_value_2)
           
         }
         
@@ -120,54 +136,85 @@ for (i in 1:nrow(new_data)) {
       
     }
     
-    if (vble %in% likert) {
+    i_data[[vble]][i_data[[vble]] %in% no_value] <- NA
+    
+    if (min(no_value) > 0) {  # & unique(!(no_value == 0))
+      
+      # get only correct data
+      i_clean_data <- i_data %>% 
+        filter(!(i_data[[vble]] %in% c(no_value)))
+      
+    } else {
+        
+      i_clean_data <- i_data
+      
+      }
+    
+    
+    if (!(vble %in% likert) & !(vble %in% idx_change_num)) {
       
       # sort posible values
       values <- unique(i_data[[vble]])
-      values <- sort(values)
       
-      # delete NA, Invalid, Missing index
-      correct_idx <- values[1:(length(values) - strange_v)]
-      
-      # Only correct index
-      value_idx <- which(i_data[[vble]] %in% correct_idx)
-      
-      # Only correct value
-      i_clean_data <- i_data[[vble]][value_idx]
-      
-      # majority class
-      freq <- table(i_clean_data)
-      max_class <- which.max(freq)
-      
-      # asign majority class
-      new_data[i, vble] <- values[max_class]
+      if (is.na(no_value)) {
+        
+        new_data[i, vble] <- NA
+        
+      } else {
+       
+        values <- sort(values)
+        # 
+        # # delete NA, Invalid, Missing index
+        # correct_idx <- values[1:(length(values) - strange_v)]
+        # 
+        # # Only correct index
+        # value_idx <- which(i_data[[vble]] %in% correct_idx)
+        # 
+        # # Only correct value
+        # i_clean_data <- i_data[[vble]][value_idx]
+        
+        # majority class
+        freq <- table(i_clean_data[[vble]])
+        max_class <- which.max(freq)
+        
+        
+        # asign majority class
+        new_data[i, vble] <- values[max_class]
+        
+      }
       
     } else {
       
-      # sort posible values
-      values <- unique(i_data[[vble]])
-      values <- sort(values)
-      
-      # delete NA, Invalid, Missing index
-      correct_idx <- values[1:(length(values) - strange_v)]
-      
-      # Only correct index
-      value_idx <- which(i_data[[vble]] %in% correct_idx)
-      
-      # Only correct value
-      i_clean_data <- i_data[[vble]][value_idx]
-      
-      # mean 
-      mean_value <- mean(as.numeric(i_clean_data))
-      
-      # assign value
-      new_data[i, vble] <- mean_value
+      if (is.na(no_value)) {
+        
+        new_data[i, vble] <- NA
+        
+      } else {
+        
+        # sort posible values
+        values <- unique(i_data[[vble]])
+        values <- sort(values)
+        
+        # # delete NA, Invalid, Missing index
+        # correct_idx <- values[1:(length(values) - strange_v)]
+        # 
+        # # Only correct index
+        # value_idx <- which(i_data[[vble]] %in% correct_idx)
+        # 
+        # # Only correct value
+        # i_clean_data <- i_data[[vble]][value_idx]
+        
+        # mean 
+        mean_value <- mean(as.numeric(i_clean_data[[vble]]))
+        
+        # assign value
+        new_data[i, vble] <- mean_value
+        
+      }
       
     } 
     
   }
-  
-  
   
 }
 
