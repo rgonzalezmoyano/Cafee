bd492f02b4b3db1772972dcc3917fb055731a10d
rgonzalezmoyano <125368117+rgonzalezmoyano@users.noreply.github.com>
2024-04-05 14:01:08 +0200

    codigo completo

--- pisa/clasification_var_function.R
+++ pisa/clasification_var_function.R
@@ -14,7 +14,7 @@ data <- a$data
 info <- a$names
 
 # assign factor variable
-idx_change_factor <- c(4, 9, 10, 11, 12, 14, 21:26, 28:31, 34:37, 39, 40:60, 120:124,
+idx_change_factor <- c(4, 9, 10, 11, 12, 14, 21:26, 28:31, 34:37, 39, 40:41, 43:60, 120:124,
                        133:136,  147:171, 284:301, 333:339, 348:376, 378:387, 389:390,
                        392, 408:410, 412:413, 421:423, 435, 439, 448, 456:459, 468:469,
                        483, 632, 634, 635)
@@ -24,7 +24,7 @@ likert <- c(8, 13, 15:20, 27, 32:33, 38, 61:119, 125:132, 137:140, 180:283, 302:
             340:347, 377, 388, 391, 393:404, 436, 439, 454:455, 465)
 
 # numeric variables 
-idx_change_num <- c(141:146, 172:179, 405:407, 411, 414:420, 424:434, 437:438, 440:447,
+idx_change_num <- c(42, 141:146, 172:179, 405:407, 411, 414:420, 424:434, 437:438, 440:447,
                     449:453, 460:464, 466, 467, 470:476, 477:482, 484:499, 500:631, 633)
 
 # test and validation variables
@@ -66,7 +66,7 @@ new_data <- as.data.frame(new_data)
 names(new_data) <- names(data)
 
 # bucle
-for (i in 23:nrow(new_data)) {
+for (i in 1:nrow(new_data)) {
   print(i)
   # ID
   new_data[i, 635] <- unique(data$ID_PISA)[i]
@@ -97,13 +97,14 @@ for (i in 23:nrow(new_data)) {
   new_data[i, 7] <- NA
   
   for (vble in 8:ncol(i_data)) {
+    #copy_data <- i_data
     
     # determinate if exist strange range
     posible_value <- unique(data[[vble]])
     
     posible_value <- sort(as.numeric(posible_value))
     
-    if (vble %in% c(11, 12, 409, 410, 457, 459, 500:630, 634, 635)) {
+    if (vble %in% c(11, 12, 34, 36:37, 120:124, 409, 410, 457, 459, 500:630, 634, 635)) {
       
       strange_v <- 0
       no_value <- 0
@@ -136,27 +137,42 @@ for (i in 23:nrow(new_data)) {
       
     }
     
+    # Transform to numeric if it is posible
+    # Check if there are any elements containing characters
+    if (!any(grepl("[a-zA-Z]", i_data[[vble]]))) {
+      
+      # Convert to numeric if there are no characters
+      i_data[[vble]] <- as.numeric(i_data[[vble]])
+      
+    }
+    
+    # imput NA on strange value
     i_data[[vble]][i_data[[vble]] %in% no_value] <- NA
     
+    # confirm no data error
+    i_data[[vble]] <- ifelse(i_data[[vble]] %in% c(9999, 9998, 9997), NA, i_data[[vble]])
+    
+    # clean data: filter no_value
     if (min(no_value) > 0) {  # & unique(!(no_value == 0))
       
       # get only correct data
-      i_clean_data <- i_data %>% 
-        filter(!(i_data[[vble]] %in% c(no_value)))
+      # i_clean_data <- i_data %>% 
+      #   filter(!(i_data[[vble]] %in% c(no_value)))
       
-    } else {
+      i_clean_data <- na.omit(i_data[[vble]])
+      
+    } else { # There is not problematic data
         
-      i_clean_data <- i_data
+      i_clean_data <- i_data[[vble]]
       
       }
     
+    # sort posible values
+    values <- unique(i_clean_data)
     
     if (!(vble %in% likert) & !(vble %in% idx_change_num)) {
       
-      # sort posible values
-      values <- unique(i_data[[vble]])
-      
-      if (is.na(no_value)) {
+      if (all(is.na(values))) {  ##### cambios np_value por values
         
         new_data[i, vble] <- NA
         
@@ -174,7 +190,7 @@ for (i in 23:nrow(new_data)) {
         # i_clean_data <- i_data[[vble]][value_idx]
         
         # majority class
-        freq <- table(i_clean_data[[vble]])
+        freq <- table(i_clean_data)
         max_class <- which.max(freq)
         
         
@@ -185,14 +201,14 @@ for (i in 23:nrow(new_data)) {
       
     } else {
       
-      if (is.na(no_value)) {
+      if (all(is.na(values))) { ###### cambio no_value por values
         
         new_data[i, vble] <- NA
         
       } else {
         
         # sort posible values
-        values <- unique(i_data[[vble]])
+        # values <- unique(i_data[[vble]])
         values <- sort(values)
         
         # # delete NA, Invalid, Missing index
@@ -205,7 +221,7 @@ for (i in 23:nrow(new_data)) {
         # i_clean_data <- i_data[[vble]][value_idx]
         
         # mean 
-        mean_value <- mean(as.numeric(i_clean_data[[vble]]))
+        mean_value <- mean(as.numeric(i_clean_data))
         
         # assign value
         new_data[i, vble] <- mean_value
@@ -217,53 +233,3 @@ for (i in 23:nrow(new_data)) {
   }
   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for (i in 6:7) {
-#   data[[i]] <- as.numeric(data[[i]])
-#   print(paste(info$vble[i], "is", info$name[i]))
-# }
-# 
-# # in
-# info_data <- data[, 1:7]
-# 
-# info_data <- info_data[-7] %>% 
-#   group_by(SCHOOLID) %>% 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# value_detection <- function (data) {
-#   
-#   data_dim <- matrix (
-#     ncol = ncol(data),
-#     nrow = 1
-#   )
-#   
-#   data_dim <- apply(data, MARGIN = 2, FUN = function(x) length(unique(x)))
-#   
-#   idx_liker <- which(data_dim < 10)
-#   
-#   data_lkr <- data[idx_liker]
-#   
-#   
-#   
-# }
-
