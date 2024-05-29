7e58e15a6a3159d78c631fad1176eae1c65f7bc4
rgonzalezmoyano <125368117+rgonzalezmoyano@users.noreply.github.com>
2024-05-29 09:14:16 +0200

    descriptive scores

--- articulo 1 XAI/descriptive scores.R
+++ articulo 1 XAI/descriptive scores.R
@@ -1,3 +1,26 @@
 ### Table descriptive
 
+scores <- information_region[[1]]
 summary(scores)
+
+DMU <- as.data.frame(
+  matrix(
+    data = NA,
+    ncol = 1,
+    nrow = nrow(scores)
+  )
+)
+DMU[1] <- 1:999
+
+names(DMU) <- "DMU"
+
+data <- cbind(DMU, scores)
+
+library(ggplot2)
+ggplot() +
+  geom_density(data = data, aes(x = svmPoly, color = "orange") ) +
+  geom_density(data = data, aes(x = nnet, color = "cyan")) +
+  theme_bw()
+  
+
+
