555c567d5ca477f37c8a16d1fc9983ca65d311b8
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2024-10-07 13:20:43 +0200

    cambios

--- articulo 1 XAI/Apuntes/Sensitivity analysis/To Cortez/example_question.R
+++ articulo 1 XAI/Apuntes/Sensitivity analysis/To Cortez/example_question.R
@@ -4,10 +4,12 @@ library("rminer")
 # load data
 load("train_data.RData")
 
+tr=train_data[,c(2:5,1)]
+
 # m model: SVM
 m <- fit(
   ClassEfficiency~.,
-  data = train_data,
+  data = tr,
   model = "ksvm",
   kernel = "polydot",
   scale = "none",
@@ -31,67 +33,16 @@ m2 = fit(
     scale = 0.1),
   C = 10
   )
-g
-I2 = Importance(m2, tr, method = "GSA", interactions = 1:4)
-cm2 = agg_matrix_imp(I2)
 
-agg_matrix_imp(
-  I2,
-  INP = I2$inputs,
-  measure=I2$measure,
-  Aggregation = I2$agg,
-  method = I2$method,
-  outdata = NULL,
-  L = I2$Llevels,
-  ameth = "xy",
-  Tolerance = 0.1
-  )
-
-aggregate_imp()
-print("show Table 8 DSA results (from the reference):")
-print(round(cm2$m1, digits=2))
-print(round(cm2$m2, digits=2))
-
-fcm2=cmatrixplot(cm2,threshold=0.1) 
-
-# the graph shows the interactions between 2 inputs, the darker, the stronger they are.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# importance variables
-importance <- Importance(
-  M = m,
-  RealL = 7, # Levels
-  data = tr, # data
+I2 = Importance(
+  m2,
+  tr,
   method = "GSA",
-  measure = "AAD",
-  interactions = 1:4,
-  responses = TRUE
+  interactions=1:4
 )
 
-importance$value
-importance$imp
-agg_matrix_imp(importance)
+cm2=agg_matrix_imp(I2)
 
+print(round(cm2$m1,digits=2))
+print(round(cm2$m2,digits=2))
+fcm2=cmatrixplot(cm2,threshold=0.1) 
