e2906b8902ce642ca53b96cfe3b90dc530fc5fc0
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2024-10-08 14:51:24 +0200

    to cortez

--- articulo 1 XAI/Apuntes/Sensitivity analysis/To Cortez/example_question.R
+++ articulo 1 XAI/Apuntes/Sensitivity analysis/To Cortez/example_question.R
@@ -46,3 +46,62 @@ cm2=agg_matrix_imp(I2)
 print(round(cm2$m1,digits=2))
 print(round(cm2$m2,digits=2))
 fcm2=cmatrixplot(cm2,threshold=0.1) 
+
+# # single input importance?
+# Inps = 4
+# val = vector(length=Inps)
+# 
+# for(a in 1:Inps){
+#   
+#   avg_imp(I2, 1 ,measure="AAD")
+#   
+#   val[i]= avg_imp(I2,i,measure="AAD")$value
+#   ximportance=val/sum(val) # vector with the % overall importance values for each inputs, from att. 2 to 5.
+#   
+# } 
+
+
+
+
+
+### 
+Inps=4
+val=vector(length=Inps)
+for (a in 1:length(I2$inputs)) {
+  AT <- a
+  x=I2$sresponses[[1]]$x
+  y=I2$sresponses[[1]]$y
+  X1=unique(x[,AT[1]])
+  print(X1)
+  
+  LX=length(X1)
+  if(is.matrix(y)) {
+    my = matrix(
+      ncol=NCOL(y),
+      nrow=LX
+    )
+  } else if(is.factor(y)){
+    my=factor(rep(levels(y)[1],LX),levels=levels(y))
+  }  else {
+    my=vector(length=LX)
+  }
+  
+  Im = vector(length=LX); k=1;
+  
+  for(i in X1) {
+    W=which(x[,AT[1]]==i)
+    Im[k]=W[1]
+    if(is.matrix(y)) my[k,]=colMeans(y[W,])
+    else my[k]=mean_resp(y[W])
+    k=k+1
+  }
+  
+  
+  
+  print(s_measure(my, "AAD"))
+  val[a] <- s_measure(my, "AAD")
+  
+  
+}
+
+ximportance=val/sum(val) # vector with the % overall importance values for each inputs, from att. 2 to 5.
