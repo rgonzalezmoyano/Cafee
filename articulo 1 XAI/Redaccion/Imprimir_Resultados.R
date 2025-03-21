735c7dd2ded8116a0e25c1e4bcdf30734ca2c99e
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2025-03-21 14:19:09 +0100

    mejoras en Tabla4

--- articulo 1 XAI/Redaccion/Imprimir_Resultados.R
+++ articulo 1 XAI/Redaccion/Imprimir_Resultados.R
@@ -1,4 +1,5 @@
 # Cargar el paquete
 library(writexl)
+load("C:/Users/Ricardo/OneDrive - UMH/Documentos/Cafee/articulo 1 XAI/code paper/resultados_art_XAI_NN_CV_1_3.RData")
 
-write_xlsx(as.datalist_method$nnet$peer_list$"0.75", "eff_vector.xlsx")
+write_xlsx(as.data.frame(list_method$nnet$peer_list$"0.95", "peer095.xlsx"))
