01b9aacac8c6feeed82dacad0422f3f07959527c
Ricardo <ricardo.gonzalez01@goumh.umh.es>
2025-03-28 12:43:46 +0100

    mejoras

--- articulo 1 XAI/Redaccion/Images/importance.R
+++ articulo 1 XAI/Redaccion/Images/importance.R
@@ -9,11 +9,11 @@ library(tools)
 # svm
 imp <- list_method[["nnet"]][["result_SA"]]
 
-names(imp)[4] <- "personnel exepenses"
+names(imp)[4] <- "personnel expenses"
 
 colnames(imp) <- gsub("_", " ", colnames(imp))
 
-colnames(imp) <- toTitleCase(gsub("_", " ", colnames(imp)))
+colnames(imp) <- sub("^(.)", "\\U\\1", tolower(colnames(imp)), perl = TRUE)
 
 data <- imp
 df_long <- gather(data, key = "variable", value = "importance")
@@ -30,8 +30,8 @@ plot <- ggplot(data = df_long, aes(x = reorder(variable, importance), y = import
   
   coord_flip() +
   
-  labs(x = "variables",
-       y = "relative importance") +
+  labs(x = "Variables",
+       y = "Relative importance") +
   
   # geom_text(aes(x = reorder(variable, importance), y = 0.08, label = variable), 
   #           hjust = -0.5, color = "black", size = 3.5) +
@@ -50,6 +50,37 @@ plot <- ggplot(data = df_long, aes(x = reorder(variable, importance), y = import
   theme(axis.text.y = element_blank(),
         panel.background = element_rect(fill = "white", color = NA),  # Fondo del panel blanco
         plot.background = element_rect(fill = "white", color = NA)) 
+
+plot <- ggplot(data = df_long, aes(x = reorder(variable, importance), y = importance)) +
+  
+  geom_bar(
+    stat = "identity",
+    fill = "white",
+    color = "black"
+  ) + 
+  
+  coord_flip() +
+  
+  labs(x = "Variables",
+       y = "Relative importance") +
+  
+  geom_text(aes(label = paste0(variable, " ", round(importance * 100, 1), "%"), 
+                y = ifelse(importance == max(importance), importance - 0.07, importance + 0.02)),  
+            hjust = ifelse(df_long$importance == max(df_long$importance), 0.5, 0),  
+            color = "black", size = 4.5) +
+  
+  geom_vline(xintercept = 0, linetype = "solid", color = "black") +
+  
+  theme_minimal() +
+  
+  theme(
+    axis.text.y = element_blank(),
+    axis.text.x = element_text(size = 14),       # Tamaño del texto del eje X
+    axis.title.x = element_text(size = 16),      # Tamaño del título del eje X
+    axis.title.y = element_text(size = 16),      # Tamaño del título del eje Y
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background = element_rect(fill = "white", color = NA)
+  )
 
 plot 
 ggsave(plot = plot, dpi = 600, width = 10, heigh = 6, filename = "1D bar plot_CV_importance.png")
